f3b2a1a10b72b34d729811f67445066f39c1178a
yuki1293 <timmy1123like@gmail.com>
2026-05-14 18:39:38 +0900

    [Add] 物理挙動
    CandyオブジェクトとPusuerオブジェクトにColliderとRigidBodyを追加しました

---
+++ Assets/Scripts/Pusher.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class Pusher : MonoBehaviour
+{
+    //アクセス修飾子
+    public float speed = 1f;
+    public float movePower = 5f;
+    private Vector3 startPosition;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        startPosition = this.transform.position;
+        Debug.Log("ゲームが開始した");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float z = Mathf.Sin(Time.time * speed) * movePower;
+
+        this.transform.localPosition =startPosition + new Vector3(0, 0, z);
+    }
+}
