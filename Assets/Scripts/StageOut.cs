a400c26f28c6f9d8b6baef448861334d80b56f6a
yuki1293 <timmy1123like@gmail.com>
2026-08-20 18:41:25 +0900

    スコア2倍

--- Assets/Scripts/StageOut.cs
+++ Assets/Scripts/StageOut.cs
@@ -1,34 +1,98 @@
 using UnityEngine;
 using TMPro;
-using System;
 
 public class StageOut : MonoBehaviour
 {
     private int Score = 0;
+
     public TextMeshProUGUI ScoreText;
 
-    // 来週の頭(一限)に適当に選んだ3人を当てる予定
-    // 要件定義：StageOutクラスの中に変数 Scoreを作成、オブジェクトがすり抜けたらScoreを1加算する
-    // 1. StageOutクラスの中に変数 Scoreを作成
-    // 2. 変数 Score は int型 かつ private であること
-    // 3. オブジェクトがすり抜けたら(OnTriggerEnterが呼ばれたら)変数 Scoreに1を加算する
-    // 4. 加算後の変数 ScoreをDebug.Logでコンソール上に出力する
+    // 落としたコインの枚数
+    private int coinCount = 0;
 
-    // このコードがアタッチされているオブジェクトのisTrigger(コライダー設定)が有効⇩
-    // かつ他のオブジェクトがすり抜けた時に中の処理を行うイベント関数⇩
+    // 何枚落としたらスコア2倍にするか
+    public int requiredCoinCount = 10;
+
+    // スコア2倍になっているか
+    private bool scoreUp = false;
+
+    // スコア2倍になる時間
+    public float scoreUpTime = 10f;
+
+    // スコア2倍の残り時間
+    private float timer = 0f;
+    // 残り時間を表示するText
+    public TextMeshProUGUI TimerText;
+
+
+    void Update()
+    {
+        Debug.Log("scoreUp = " + scoreUp);
+
+        if (scoreUp)
+        {
+            timer -= Time.deltaTime;
+
+            TimerText.text =
+                "スコア2倍 残り：" + Mathf.Ceil(timer) + "秒";
+
+            Debug.Log("timer = " + timer);
+
+            if (timer <= 0)
+            {
+                scoreUp = false;
+                TimerText.text = "";
+
+                Debug.Log("スコア2倍終了！");
+            }
+        }
+    }
+
+
     void OnTriggerEnter(Collider other)
     {
         AudioManager.instance.SEPlay(0);
 
-        Score += 1;
+        // 落としたコインの数を1増やす
+        coinCount++;
+
+        Debug.Log("落としたコイン：" + coinCount);
+
+        // スコアを追加する
+        int addScore = 1;
+
+        // スコア2倍中なら2点
+        if (scoreUp)
+        {
+            addScore = 2;
+        }
+
+        Score += addScore;
+
         ScoreText.text = $"スコア：{Score}";
-        // 変数名 otherってなに？
-        // A.すり抜けた相手のコライダー情報
+
         Debug.Log($"{other.name}がすり抜けました。");
-        // Destroy関数
-        // Destroy(破棄したいオブジェクト)
-        // オブジェクトが使用しているメモリの解放(ガベージコレクション)と描画情報の破棄
+
+        // 一定数落としたか確認
+        if (coinCount >= requiredCoinCount)
+        {
+            StartScoreUp();
+
+            // カウントを0に戻す
+            coinCount = 0;
+        }
+
         Destroy(other.gameObject);
     }
-}
 
+
+    // スコア2倍を開始する
+    public void StartScoreUp()
+    {
+        scoreUp = true;
+
+        timer = scoreUpTime;
+
+        Debug.Log("スコア2倍開始！");
+    }
+}
